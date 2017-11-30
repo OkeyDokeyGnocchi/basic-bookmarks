9f63c29c99ffc50a468555dbd888954ab989915c
OkeyDokeyGnocchi <okeydokeygnocchi@gmail.com>
2017-11-29 16:24:24 -0800

    Use PORT from environment.
    Add import os.
    Add PORT env variable to replace forced port 8000.

--- BookmarkServer.py
+++ BookmarkServer.py
@@ -1,49 +1,7 @@
-#!/usr/bin/env python3
-#
-# A *bookmark server* or URI shortener that maintains a mapping (dictionary)
-# between short names and long URIs, checking that each new URI added to the
-# mapping actually works (i.e. returns a 200 OK).
-#
-# This server is intended to serve three kinds of requests:
-#
-#   * A GET request to the / (root) path.  The server returns a form allowing
-#     the user to submit a new name/URI pairing.  The form also includes a
-#     listing of all the known pairings.
-#   * A POST request containing "longuri" and "shortname" fields.  The server
-#     checks that the URI is valid (by requesting it), and if so, stores the
-#     mapping from shortname to longuri in its dictionary.  The server then
-#     redirects back to the root path.
-#   * A GET request whose path contains a short name.  The server looks up
-#     that short name in its dictionary and redirects to the corresponding
-#     long URI.
-#
-# Your job in this exercise is to finish the server code.
-#
-# Here are the steps you need to complete:
-#
-# 1. Write the CheckURI function, which takes a URI and returns True if a
-#    request to that URI returns a 200 OK, and False otherwise.
-#
-# 2. Write the code inside do_GET that sends a 303 redirect to a known name.
-#
-# 3. Write the code inside do_POST that sends a 400 error if the form fields
-#    are missing.
-#
-# 4. Write the code inside do_POST that sends a 303 redirect to the form
-#    after saving a newly submitted URI.
-#
-# 5. Write the code inside do_POST that sends a 404 error if a URI is not
-#    successfully checked (i.e. if CheckURI returns false).
-#
-# In each step, you'll need to delete a line of code that raises the
-# NotImplementedError exception.  These are there as placeholders in the
-# starter code.
-#
-# After writing each step, restart the server and run test.py to test it.
-
 import http.server
 import requests
 from urllib.parse import unquote, parse_qs
+import os
 
 memory = {}
 
@@ -147,6 +105,7 @@ class Shortener(http.server.BaseHTTPRequestHandler):
                 "Error 404, URI '{}' not found!".format(longuri).encode())
 
 if __name__ == '__main__':
-    server_address = ('', 8000)
+    port = int(os.environ.get('PORT', 8000))
+    server_address = ('', port)
     httpd = http.server.HTTPServer(server_address, Shortener)
     httpd.serve_forever()
